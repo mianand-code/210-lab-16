a5badd16ca99010ae22b67c1b5c378f4d27007e3
Mira Anand <miraanand@comcast.net>
2024-10-02 11:43:50 -0700

    10 minute update - 3

--- Lab16MiraAnand.cpp
+++ Lab16MiraAnand.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 // declaration and initialization of a const int variable to represent the maximum RGB hue value
-// creation of this variable will help to generate a random number for each RGB hue value in main()
+// creation of this variable will help to generate a random number for an object's RGB hue values in main()
 // initializing the variable with the value 256 because rand() % 256 = 0-255, which is the range of possible values for each hue according to the RGB color model
 const int MAX_HUE_VALUE = 256;
 // declaration and initialization of a const int variable to use with setw() to neatly format output
@@ -84,37 +84,26 @@ class Color
 
 int main()
 {
-    srand(time(0)); // needed as the first line in main() to generate random number
+    srand(time(0)); // needed as the first line in main() to generate random numbers for the RGB hue values for color4 object
 
-    // creating each Color object separately in order to gain practice with creating various objects and with using the dot operator to access public member functions
+    // creating each Color object separately in order to gain practice with creating various objects and with using the dot operator to access public member functions (print())
     
     // creation of 1st Color object
+    // this object relates to the default constructor - since all RGB hue values were initialized to 0, all RGB hue values will print as 0
     Color color1;
-    // using setters to populate the object with data
-    // populating the object with random number data, rand() % MAX_HUE_VALUE = 0-255
-    color1.setRed(rand() % MAX_HUE_VALUE);
-    color1.setGreen(rand() % MAX_HUE_VALUE);
-    color1.setBlue(rand() % MAX_HUE_VALUE);
     // outputting hue values to the console using the object's print() method
     color1.print();
 
     // creation of 2nd Color object
-    Color color2;
-    // using setters to populate the object with data
-    // populating the object with random number data, rand() % MAX_HUE_VALUE = 0-255
-    color2.setRed(rand() % MAX_HUE_VALUE);
-    color2.setGreen(rand() % MAX_HUE_VALUE);
-    color2.setBlue(rand() % MAX_HUE_VALUE);
+    // this object relates to the first partial constructor
+    // since "red" was initialized by parameter and "blue" and "green" initialized to 0, "red" will print as 100 and "blue" and "green" as 0
+    // you can set "red" to any hue value you would like by replacing the 100 with another value
+    Color color2(100);
     // outputting hue values to the console using the object's print() method
     color2.print();
 
     // creation of 3rd Color object
     Color color3;
-    // using setters to populate the object with data
-    // populating the object with random number data, rand() % MAX_HUE_VALUE = 0-255
-    color3.setRed(rand() % MAX_HUE_VALUE);
-    color3.setGreen(rand() % MAX_HUE_VALUE);
-    color3.setBlue(rand() % MAX_HUE_VALUE);
     // outputting hue values to the console using the object's print() method
     color3.print();
 
